fee977a1f08d55ee687550728694e1fe4bb797cb
Sazied <mshassan@mun.ca>
2021-03-31 15:09:13 -0230

    changes

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -5,10 +5,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () =>
 {
-  const { logout } = useAuth0();
+  const { logoutWithRedirect } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button onClick={() => logoutWithRedirect()}>
       Log Out
     </button>
   );
