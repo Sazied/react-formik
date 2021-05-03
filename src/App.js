ed1e04ba7c733b4bff842ea1807718c566178d16
Sazied <mshassan@mun.ca>
2021-05-03 13:50:12 -0230

    changes

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App()
   const { user, isAuthenticated, isLoading } = useAuth0();
   
   
-  const location = window.location.host.includes('github.io');
+  const location = window.location.host.includes('github.ioss');
   
   function isAuth()
   {
